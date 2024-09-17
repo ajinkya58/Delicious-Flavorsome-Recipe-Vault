73568c50c00d866b3cb431cdf58c1802f85a6321
Ajinkya Nanivadekar <126323636+ajinkya58@users.noreply.github.com>
2024-09-17 20:02:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './components/styles.css';
+import Meal from './components/Meal';
+function App() {
+  return(
+      <Meal/>
+  )
+  
+}
+
+export default App;
+
